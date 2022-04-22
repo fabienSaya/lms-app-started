809d10afc8c058cc33c8e065e3ab55b6d979f89e
fabienSAYA <fsayatravail@hotmail.com>
2022-04-22 19:18:03 +0200

    autre maniere d'ecrire

--- src/app/shared/interceptors/loader.interceptor.ts
+++ src/app/shared/interceptors/loader.interceptor.ts
@@ -5,7 +5,7 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { finalize, Observable } from 'rxjs';
+import { finalize, Observable, tap } from 'rxjs';
 import { LoaderService } from '../services/loader.service';
 
 @Injectable()
@@ -17,8 +17,14 @@ export class LoaderInterceptor implements HttpInterceptor {
     //la request arrive donc on indique qu'on démarre un load (ie une requete)
     this.loaderSvc.setLoader(true);
 
+    //les 2 écritures fonctionnent (chez moi). Finalize ou complete.
     return next.handle(request).pipe(
-      finalize(()=> this.loaderSvc.setLoader(false)) //on met à faux une fois que la requete est terminée
+      //finalize(()=> this.loaderSvc.setLoader(false)) //on met à faux une fois que la requete est terminée
+      tap ({//next et error ne servent à rien ici mais c'est pour montrer qu'on pourrait les utiliser si on voulait.
+        next: () => {},
+        error: () => {},
+        complete: () => this.loaderSvc.setLoader(false)
+      })
     );
   }
 }
